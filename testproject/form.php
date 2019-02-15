7289632456a225d8373da8a0c9415049658d437d
VasilevAlexey <Lexich.3d@gmail.com>
2019-02-15 15:58:19 +0300

    first commit

---
+++ testproject/form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h3>Вход на сайт</h3>
+<form action="login.php" method="POST">
+    Логин: <input type="text" name="login" /><br><br>
+    Пароль: <input type="text" name="password" /><br><br>
+    <input type="submit" value="Войти">
+</form>
+<script type="text/javascript">(function() {window.tokenCallbackNeirika = 'JTOwHBwPNGP7rU7p8aLmVW4Qtc90jfE'; window.locNeirika = 'https://system.neirika.ru';  window.widgetKey='abcde'; var script = document.createElement('script');script.type = 'text/javascript'; script.async = true; script.src = window.locNeirika+"/assets/widgets/callback/horizontal/widget.js"; document.getElementsByTagName('head')[0].appendChild(script); })();</script>
+</body>
+</html>
