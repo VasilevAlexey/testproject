51d5f68d3b306dff55a3d6e3283964ff3fd8cc3d
VasilevAlexey <Lexich.3d@gmail.com>
2019-02-18 12:37:07 +0300

    almost REST Api

---
+++ testproject/CalculationApi.php
@@ -0,0 +1,72 @@
+<?php
+require_once 'Api.php';
+require_once 'CalculationApi.php';
+
+class CalculationApi extends Api
+{
+    public $apiName = 'calculation';
+
+    /**
+     * Метод GET
+     * Заглушка
+     */
+    public function indexAction(){
+        echo "Method Get";
+    }
+
+
+    /**
+     * Метод POST
+     * заглушка
+     */
+    public function viewAction() {
+        {
+
+
+            echo "Method POST, view";
+        }
+    }
+
+    /**
+     * Метод POST
+     * Вычисления
+     */
+    public function sumAction() {
+        {
+            $a = (int)$this->requestParams["a"];
+            $b = (int)$this->requestParams["b"];
+            $c = $a + $b;
+            $fOut = ".";
+            if ($c >= 0) {
+                $factorial = $this->factorial($c);
+                $fOut =  ". Её факториал равен " . (string)$factorial . ".";
+            }
+            $outString = "Сумма чисел равна " . (string)$c . $fOut;
+            echo $outString;
+        }
+    }
+
+    public function factorial($n)
+    {
+        if ($n == 0) {
+            return 1;
+        } else {
+            return $n * $this->factorial($n - 1);
+        }
+    }
+
+    /**
+     * Метод PUT
+     * Заглушка
+     */
+    public function updateAction() {
+        echo "Method PUT";
+    }
+
+    /**
+     * Метод DELETE
+     * Заглушка
+     */
+    public function deleteAction() {}
+
+}
---
+++ testproject/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h3>Вход на сайт</h3>
+<form action="index.php" method="POST">
+    a: <input type="text" name="a" /><br><br>
+    b: <input type="text" name="b" /><br><br>
+    <input type="submit" value="Войти">
+</form>
+<?php
+
+require_once 'CalculationApi.php';
+
+try {
+    $api = new calculationApi();
+    echo $api->run();
+} catch (Exception $e) {
+    echo json_encode(Array('error' => $e->getMessage()));
+} ?>
+</body>
+</html>
